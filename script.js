8af77d9ff255b39fcdeaf578546dcc26dd3558dc
Nam <62420404+HoangNamDo@users.noreply.github.com>
2020-06-17 00:27:35 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,27 +6,29 @@ function checkEligibility() {
   let dateStamp = new Date(2020, 06, 01);
 
   let age = calculateAge(dateOfBirth, dateStamp);
-  const name = document.getElementById("name").value;
+  let name = document.getElementById("name").value;
 
-  let greeting = `Hi ${name}! You are ${age} years of age as of July 1, 2020.`;
-  let result = "";
-
-  if (age == 16 || age == 17)
-    result = "Congratulation! You are allowed to take classes during Summer Quarter by the ACME College Summer Program.";
-  else
-    result = "Sorry! You are NOT allowed to take classes during Summer Quarter by the ACME College Summer Program.";
-
+  let greeting = "Hi " + name + "! You are " + age + " years of age as of July 1, 2020.";
   document.getElementById("greeting").innerHTML = greeting;
+
+  let result = "";
+  if (age == 16 || age == 17) {
+    result = "Congratulation! You are allowed to take classes during Summer Quarter by the ACME College Summer Program.";
+  } else {
+    result = "Sorry! You are NOT allowed to take classes during Summer Quarter by the ACME College Summer Program.";
+  }
   document.getElementById("result").innerHTML = result;
 }
 
 function calculateAge(firstDate, secondDate) {
-  diffYear = secondDate.getFullYear() - firstDate.getFullYear();
-  diffMonth = secondDate.getMonth() - firstDate.getMonth();
-  diffDate = secondDate.getDate() - firstDate.getDate();
-
-  if ((diffMonth > 0) || (diffMonth = 0 && diffDate > 0))
-    return diffYear;
-  else
-    return diffYear - 1;
+  let diffYear = secondDate.getFullYear() - firstDate.getFullYear();
+  let diffMonth = secondDate.getMonth() - firstDate.getMonth();
+  let diffDate = secondDate.getDate() - firstDate.getDate();
+  let _age;
+  if ((diffMonth > 0) || (diffMonth = 0 && diffDate > 0)) {
+    _age = diffYear;
+  } else {
+    _age = diffYear - 1;
+  }
+  return _age;
 }
